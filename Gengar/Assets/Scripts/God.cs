b8a36701f7701adc950e8a2efffe6e5f3953e0f1
wzhu <macbookair@rrcs-108-169-151-40.midsouth.biz.rr.com>
2017-04-02 00:46:04 -0400

    environment now contain generated food and obstacle

--- Gengar/Assets/Scripts/God.cs
+++ Gengar/Assets/Scripts/God.cs
@@ -2,16 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using UnityEditor; // Contains the PrefabUtility class.
+
 public class God : MonoBehaviour {
 
+	public Transform env;
 	public Transform brick;
+	public Transform food;
 
-	void Start() {
-		int quant = 100;
+	public GameObject tosave;
+
+	[MenuItem ("My Project/Create Simple Prefab")]
+	 void saveEnv()
+	{
+		PrefabUtility.CreatePrefab("Assets/Prefabs/Whatever.prefab", 
+			tosave);
+	}
+
+	void putOnGround(Transform obj, int quant, float y) {
 
 		for (int i = 0; i < quant; i++) {
-			Instantiate(brick, new Vector3(getGoodRand(), 1, getGoodRand()), Quaternion.identity);
+			Instantiate(obj, new Vector3(getGoodRand(), y, getGoodRand()), Quaternion.identity, env);
+
 		}
+	}
+
+	void Start() {
+		putOnGround (brick, 30, 0.5f);
+		putOnGround (food, 60, 0);
+		saveEnv ();
 	}
 
 	int getGoodRand() {
